5c853bc85d6a27ee0866769f2d16caa90edcc3ac
rony <ahr020532@gmail.com>
2024-02-17 18:59:11 +0900

    회원정보 수정,로그인,하단바

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:readbook_hr/screens/auth.dart';
 import 'package:readbook_hr/screens/select.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyProfileScreen extends StatefulWidget {
   const MyProfileScreen({Key? key}) : super(key: key);
@@ -13,7 +16,9 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmNewPasswordController =
+      TextEditingController();
 
   @override
   void initState() {
@@ -21,17 +26,23 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
     _fetchUserData();
   }
 
-// 정보 가져오기
+  // 정보 가져오기
   Future<void> _fetchUserData() async {
     const String apiUrl = 'http://152.69.225.60/myinfo';
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token') ?? '';
+
     try {
-      final userId = 1;
-      final response = await http.get(Uri.parse(apiUrl), headers: {
-        "Content-Type": "application/json",
-      });
+      final response = await http.get(
+        Uri.parse(apiUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $token",
+        },
+      );
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body);
+        final data = json.decode(response.body);
         setState(() {
           _nameController.text = data['name'];
           _emailController.text = data['email'];
@@ -44,34 +55,75 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
     }
   }
 
-//수정 후 저장
+  // 수정 후 저장
   Future<void> _saveProfile() async {
-    String apiUrl =
-        'http://152.69.225.60/updateNick'; // Replace with your API URL
+    String apiUrl = 'http://152.69.225.60/updatemyinfo';
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token') ?? '';
 
     Map<String, String> requestBody = {
-      'email': _emailController.text,
-      'password': _passwordController.text,
       'name': _nameController.text,
+      'email': _emailController.text,
+      'password': _newPasswordController.text,
     };
 
-    var response = await http.post(
-      Uri.parse(apiUrl),
-      headers: {"Content-Type": "application/json"},
-      body: json.encode(requestBody),
-    );
-
-    if (response.statusCode == 200) {
-      // Handle success
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('수정완료하였습니다.')),
+    try {
+      var response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $token", // 토큰 사용
+        },
+        body: json.encode(requestBody),
       );
-    } else {
-      // Handle failure
+
+      if (response.statusCode == 200) {
+        _showSuccessDialog(); // 성공 모달 표시
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to update profile')),
+        );
+      }
+    } catch (e) {
+      print('Error occurred: $e');
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('실패하였습니다.')),
+        const SnackBar(content: Text('Error occurred while updating profile')),
       );
     }
+  }
+
+  void _showSuccessDialog() {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('Profile Updated'),
+        content: const Text('Your profile has been updated successfully.'),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(ctx).pop(); // 모달 닫기
+              _logout(); // 로그아웃 함수 호출
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _logout() async {
+    // SharedPreferences에서 토큰 제거
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+
+    // 로그인 화면으로 이동
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+          builder: (context) => AuthScreen(
+                isLogin: true,
+              )),
+    );
   }
 
   @override
@@ -84,10 +136,15 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
           style: TextStyle(color: Colors.black),
         ),
         iconTheme: const IconThemeData(color: Colors.black),
-        // leading: IconButton(
-        //   icon: const Icon(Icons.arrow_back, color: Colors.black),
-        //   onPressed: () => Navigator.of(context).pop(),
-        // ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            // SelectScreen으로 이동
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (_) => const SelectScreen()),
+            );
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
@@ -124,11 +181,27 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
               ),
             ),
             const SizedBox(height: 20),
-            Text('password',
+            Text('new password',
                 style: TextStyle(color: Colors.grey[600], fontSize: 16)),
             const SizedBox(height: 8),
             TextField(
-              controller: _passwordController,
+              controller: _newPasswordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+                fillColor: Colors.grey[200],
+                filled: true,
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            Text('confirm new password',
+                style: TextStyle(color: Colors.grey[600], fontSize: 16)),
+            const SizedBox(height: 8),
+            TextField(
+              controller: _confirmNewPasswordController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8.0),
@@ -141,9 +214,7 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
             ),
             const SizedBox(height: 40),
             ElevatedButton(
-              onPressed: () {
-                // Implement save functionality
-              },
+              onPressed: _saveProfile, // 프로필 저장 기능 연결
               child: Text('저장', style: TextStyle(fontSize: 16)),
               style: ElevatedButton.styleFrom(
                 primary: Colors.green,
---
+++ lib/widgets/navbar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  final int selectedIndex;
+  final Function(int) onItemSelected;
+
+  const CustomBottomNavigationBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  }) : super(key: key);
+
+  @override
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book_outlined),
+          label: 'Profile',
+        ),
+        // 추가 탭 아이템들
+      ],
+      currentIndex: widget.selectedIndex,
+      selectedItemColor: Theme.of(context).primaryColor,
+      onTap: widget.onItemSelected,
+    );
+  }
+}
