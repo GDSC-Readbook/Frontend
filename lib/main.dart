b47544ca22b63ed81943f55240b5d99527f0e238
Batjimin <147qqaa@hufs.ac.kr>
2024-02-19 00:53:48 +0900

    2024-02-19 박지민 파이어베이스 코드 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:readbook_hr/widgets/navbar.dart';
-import 'firebase_options.dart';
+
 import 'package:readbook_hr/screens/select.dart';
 import 'package:readbook_hr/screens/splash.dart';
 // import 'package:readbook_hr/screens/playing.dart';
@@ -11,9 +10,7 @@ import 'package:readbook_hr/screens/start.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
   runApp(const MyApp());
 }
 
